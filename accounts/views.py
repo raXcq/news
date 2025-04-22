60e7ba0facf3ab41bb6f43616164217413572689
Racque Cordeta <66395124+Ghostcoder0427@users.noreply.github.com>
2025-04-23 01:36:42 +0800

    user authentication

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
+from .forms import CustomUserCreationForm
+
 
 # Create your views here.
+class SignUpView(CreateView):
+    form_class = CustomUserCreationForm
+    success_url = reverse_lazy(
+        "login"
+    )  # Redirect to the login page after successful signup
+    template_name = "registration/signup.html"  # Path to your signup template
